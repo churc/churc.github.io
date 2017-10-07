64f415434d8a48c56ce720a2f2fa9835f1863cdd
churc <ccinflux@hotmail.com>
2017-10-07 02:02:19 -0400

    Update main.js

--- parsonsjs/main.js
+++ parsonsjs/main.js
@@ -1,1 +1,10 @@
 alert("wassup")
+var grammarObj = { "S":["The #placement# of an #object# or #shape# may affect the sense of #uncertainty#"],"placement":["high", "low", "right", "left", "small", "falling off"],"object":["animal", "iglow", "elephant", "ear", "eye", "anteater"],"shape":["circle", "dot", "plane", "area", "blob", "line"],"uncertainty":["uncertainty", "unease", "anxiety", "emptiness", "chaos", "confusion"]
+}
+
+var grammar = tracery.createGrammar(grammarObj)
+
+function generate(){
+	var t = grammar.flatten("#S#");
+	$('h1').text(t);
+}
