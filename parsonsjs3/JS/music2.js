bf106ec84dd4358284bfeee84b20746a335942f4
churc <ccinflux@hotmail.com>
2017-10-24 08:46:10 -0400

    Add files via upload

--- parsonsjs3/JS/music2.js
+++ parsonsjs3/JS/music2.js
@@ -32,7 +32,6 @@ myCanvas.parent('myContainersound');
 }
 
 function draw() {
-//  background(51, 173, 255);
 	background(153, 255, 255);
 
   /** 
@@ -60,7 +59,7 @@ function keyPressed() {
 }
 
 
-//////ADD lyrics show when key is pressed
+//////This works to ADD lyrics show when button is pressed
 
 //markov works Amy Winehouse lyrics  Mark Ronson, Amy Winehouse Back To Black
 
@@ -86,6 +85,78 @@ $('#headerMark').text(genPnP)
 //}
 
 
+
+///////MIC works to record and play back sound but doesn't can't see myContainersound anymore
+
+var mic, recorder, soundFile;
+
+var state = 0; // mousePress will increment from Record, to Stop, to Play
+
+function setup() {
+  myCanvas2 = createCanvas(1200,300);
+	myCanvas2.parent('myContainerdraw');
+  background(200);
+  fill(0);
+  text('Enable mic and click the mouse to begin recording', 20, 20);
+
+  // create an audio in
+  mic = new p5.AudioIn();
+
+  // users must manually enable their browser microphone for recording to work properly!
+  mic.start();
+
+  // create a sound recorder
+  recorder = new p5.SoundRecorder();
+
+  // connect the mic to the recorder
+  recorder.setInput(mic);
+
+  // create an empty sound file that we will use to playback the recording
+  soundFile = new p5.SoundFile();
+}
+
+function draw() {
+  // Get the overall volume (between 0 and 1.0)
+  var vol = mic.getLevel();
+  console.log(vol)
+  fill(0,255,255);
+  noStroke();
+
+  // Draw an ellipse with height based on volume
+  var h = map(vol, 0, 1, 0, 150);
+  ellipse(width/2, height/2, 50 + h, 50 + h);
+}
+
+function mousePressed() {
+//getWave();
+  // use the '.enabled' boolean to make sure user enabled the mic (otherwise we'd record silence)
+  if (state === 0 && mic.enabled) {
+
+    // Tell recorder to record to a p5.SoundFile which we will use for playback
+    recorder.record(soundFile);
+
+    background(255,0,0);
+    text('Recording now! Click to stop.', 20, 20);
+    state++;
+  }
+
+  else if (state === 1) {
+    recorder.stop(); // stop recorder, and send the result to soundFile
+
+    background(0,255,0);
+    text('Recording stopped. Click to play & save', 20, 20);
+    state++;
+  }
+
+  else if (state === 2) {
+    soundFile.play(); // play the result!
+    saveSound(soundFile, 'mySound.wav'); // save file
+    state++;
+  }
+}
+
+
+///////////NOT USING: PARK
 ////this works to play song
 ////FILTER LOWPASS
 /*
